08af60cc619d1b992783970dfc905470a9f1a26d
Bluette1 <marylene.sawyer@gmail.com>
2020-10-09 19:46:41 +0200

    Change the name of output folder to `dist` for deploy

--- webpack.config.js
+++ webpack.config.js
@@ -6,23 +6,23 @@ module.exports = {
   entry: './src/js/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'build'),
+    path: path.join(__dirname, 'dist'),
   },
   module: {
     rules: [{
-      test: /\.js/,
-      use: 'babel-loader',
-    },
-    {
-      test: /\.css/,
-      use: ['style-loader', 'css-loader'],
-    },
-    {
-      test: /\.(png|svg|jpg|gif|jpeg|)$/,
-      use: [
-        'file-loader',
-      ],
-    },
+        test: /\.js/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|jpeg|)$/,
+        use: [
+          'file-loader',
+        ],
+      },
     ],
   },
   plugins: [
